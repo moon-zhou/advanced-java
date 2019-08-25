cfecbf82f7c353bc0388873815b2a9c3d5274ec0
moon-zhou <ayimin1989@163.com>
2019-08-25 11:04:54 +0800

    feat: cglib dynamic proxy v1 simple cglib demo

---
+++ src/main/java/org/moonzhou/proxy/dynamicproxy/cglibproxy/v1/CglibProxy.java
@@ -0,0 +1,34 @@
+package org.moonzhou.proxy.dynamicproxy.cglibproxy.v1;
+
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Description cglib实现动态代理
+ *              与jdk动态代理的第一个版本一样，代理时处理的业务逻辑侵入了具体代理里，需要进行解耦
+ * @Author moon-zhou <ayimin1989@163.com>
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2019/8/25
+ */
+public class CglibProxy implements MethodInterceptor {
+
+    /**
+     * sub：cglib生成的代理对象
+     * method：被代理对象方法
+     * args：方法入参
+     * methodProxy: 代理方法
+     */
+    @Override
+    public Object intercept(Object sub, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        System.out.println("cglib proxy before method...");
+
+        Object result = methodProxy.invokeSuper(sub, args);
+
+        System.out.println("cglib proxy after method...");
+
+        return result;
+    }
+}
---
+++ src/test/java/org/moonzhou/proxy/dynamicproxy/cglibproxy/CglibProxyTest.java
@@ -0,0 +1,38 @@
+package org.moonzhou.proxy.dynamicproxy.cglibproxy;
+import	java.util.Properties;
+
+import net.sf.cglib.core.DebuggingClassWriter;
+import net.sf.cglib.proxy.Enhancer;
+import org.junit.Test;
+import org.moonzhou.proxy.CommonBiz;
+import org.moonzhou.proxy.dynamicproxy.cglibproxy.v1.CglibProxy;
+
+/**
+ * 测试cglib实现的动态代理
+ */
+public class CglibProxyTest {
+
+    @Test
+    public void testV1Run() {
+        Properties sourceCodePath = new Properties();
+        sourceCodePath.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "E:\\TestCode\\cglib");
+
+        // 代理类class文件存入本地磁盘方便我们反编译查看源码
+        System.setProperties(sourceCodePath);
+
+        // 通过cglib动态代理获取代理对象过程(类比JDK动态代理里的Proxy，但Enhancer更强大，既可以代理接口，也可以代理普通类，但不能拦截final方法)
+        Enhancer enhancer = new Enhancer();
+
+        // 设置enhancer对象的父类，即业务类(需要被代理的类)
+        enhancer.setSuperclass(CommonBiz.class);
+
+        // 设置enhancer的回调对象
+        enhancer.setCallback(new CglibProxy());
+
+        // 创建代理对象
+        CommonBiz commonBizProxy = (CommonBiz) enhancer.create();
+
+        // 通过代理对象调用目标方法
+        commonBizProxy.execute();
+    }
+}
