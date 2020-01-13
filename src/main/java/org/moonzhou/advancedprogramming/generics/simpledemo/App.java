134c9ca7cd011cc79c6afd026c5eabfe2f780b05
moon-zhou <ayimin1989@163.com>
2020-01-13 21:45:59 +0800

    refactor: 调整泛型类注释和命名

--- src/main/java/org/moonzhou/advancedprogramming/generics/simpledemo/App.java
+++ src/main/java/org/moonzhou/advancedprogramming/generics/simpledemo/App.java
@@ -1,7 +1,5 @@
 package org.moonzhou.advancedprogramming.generics.simpledemo;
 
-import sun.rmi.runtime.Log;
-
 /**
  * @Description 测试泛型类
  * @Author moon-zhou <ayimin1989@163.com>
@@ -18,17 +16,17 @@ public class App {
     private static void testGenericClass() {
         // 泛型的类型参数只能是类类型（包括自定义类），不能是简单类型
         // 传入的实参类型需与泛型的类型参数类型相同，即为Integer.
-        Generic<Integer> genericInteger = new Generic<Integer>(123456);
+        GenericClass<Integer> genericInteger = new GenericClass<Integer>(123456);
         Integer integerKey = genericInteger.getKey();
         System.out.println("泛型测试, 申明泛型类型为Integer, key is " + integerKey);
 
         // 传入的实参类型需与泛型的类型参数类型相同，即为String.
-        Generic<String> genericString = new Generic<String>("key-value");
+        GenericClass<String> genericString = new GenericClass<String>("key-value");
         String stringKey = genericString.getKey();
         System.out.println("泛型测试, 申明泛型类型为String, key is " + stringKey);
 
         // 如果不申明泛型类型，获取值时就需要进行显示的转换
-        Generic param = new Generic("noDominantType");
+        GenericClass param = new GenericClass("noDominantType");
         String noDominantType = (String) param.getKey();
         System.out.println("泛型测试, 未申明泛型类型, 强制转换为String, key is " + noDominantType);
     }
