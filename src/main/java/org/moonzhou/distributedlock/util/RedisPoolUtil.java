966687e251fd6f6ed41fcbfad1d32e7dc0a53b4c
moon-zhou <ayimin1989@163.com>
2020-03-15 20:29:32 +0800

    feat: jedis操作抽取公共方法
    1. 调用set方法添加SetParams
    2. 更新jedis版本
    3. 添加方法备注

--- src/main/java/org/moonzhou/distributedlock/util/RedisPoolUtil.java
+++ src/main/java/org/moonzhou/distributedlock/util/RedisPoolUtil.java
@@ -1,6 +1,7 @@
 package org.moonzhou.distributedlock.util;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
 
 /**
  * redis常规操作
@@ -13,8 +14,11 @@ public class RedisPoolUtil {
     private RedisPoolUtil() {
     }
 
-    private static RedisPool redisPool;
-
+    /**
+     * get String 值
+     * @param key
+     * @return
+     */
     public static String get(String key) {
         Jedis jedis = null;
         String result = null;
@@ -31,6 +35,61 @@ public class RedisPoolUtil {
         }
     }
 
+    /**
+     * 设置String值，成功返回OK
+     *     核心点是设置值，原来没有这个key则添加，原来有值则更新这个key，操作成功了返回OK
+     * @param key
+     * @param value
+     * @return
+     */
+    public static String set(String key, String value) {
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedisResource();
+            result = jedis.set(key, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jedis.close();
+
+            return result;
+        }
+    }
+
+    /**
+     * 设置String值，同时可以设置参数
+     *     String result = jedis.set(realKey, value, "NX", "PX", lockExpireTimeOut);
+     *
+     * 不存在则设置，设置成功返回OK，如果存在，则不设置，返回null
+     * @param key
+     * @param value
+     * @param setParams
+     * @return
+     */
+    public static String set(String key, String value, SetParams setParams) {
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedisResource();
+            result = jedis.set(key, value, setParams);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jedis.close();
+
+            return result;
+        }
+    }
+
+    /**
+     * 单纯的setnx，不存在设置，同时返回1，存在则不设置返回0
+     * @param key
+     * @param value
+     * @return
+     */
     public static Long setnx(String key, String value) {
         Jedis jedis = null;
         Long result = null;
--- src/test/java/org/moonzhou/distributedlock/util/RedisPoolUtilTest.java
+++ src/test/java/org/moonzhou/distributedlock/util/RedisPoolUtilTest.java
@@ -2,14 +2,19 @@ package org.moonzhou.distributedlock.util;
 
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
 
 import static org.junit.Assert.*;
 
 public class RedisPoolUtilTest {
 
     public static final String OK = "OK";
+
     String firstKey = "moon";
     String firstVal = "handSomeBoy";
+
+    String secondKey = "hou";
+    String secondValue = "cool";
 
     @Test
     public void easyTest() {
@@ -25,15 +30,28 @@ public class RedisPoolUtilTest {
 
         String moonResult = jedis.get(firstKey);
         assertEquals(firstVal, moonResult);
+
+        jedis.close();
     }
 
     @Test
-    public void test() {
-        RedisPoolUtil.setnx(firstKey, firstVal);
+    public void testSetnx() {
+        Long result = RedisPoolUtil.setnx(firstKey, firstVal);
 
+        System.out.println(result);
         String moonVal = RedisPoolUtil.get(firstKey);
 
         assertEquals(firstVal, moonVal);
+    }
+
+    @Test
+    public void testSet() {
+        SetParams setParams = new SetParams();
+        setParams.nx();
+        setParams.px(20000);
+        String  result = RedisPoolUtil.set(secondKey, secondValue, setParams);
+
+        System.out.println(result);
     }
 
 }
