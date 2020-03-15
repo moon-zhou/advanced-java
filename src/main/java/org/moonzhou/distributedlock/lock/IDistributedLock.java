bccb8cbc26377eb5fbb2953b698200545d77d2bd
moon-zhou <ayimin1989@163.com>
2020-03-15 21:58:10 +0800

    feat: redis方案的简单分布式锁
    1. setnx加锁
    2. lua脚本解锁

--- src/main/java/org/moonzhou/distributedlock/lock/IDistributedLock.java
+++ src/main/java/org/moonzhou/distributedlock/lock/IDistributedLock.java
@@ -1,4 +1,4 @@
-package org.moonzhou.lock;
+package org.moonzhou.distributedlock.lock;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,17 +12,27 @@ import java.util.concurrent.TimeUnit;
 public interface IDistributedLock {
 
     /**
-     * 加锁
+     * 加锁:获取锁直到成功
+     * 成功lockId，存储在具体方案的抽象的实力对象里，用来解锁
      */
     void lock();
 
     /**
-     * 尝试加锁
-     * @return
+     * 尝试加锁，如果在设定时间内没获取成功，返回
+     *     成功：具体方案抽象的实力对象里记录lockId
+     *     失败：记录null
      */
-    boolean tryLock();
+    void tryLock();
 
-    boolean tryLock(long time, TimeUnit unit) throws InterruptedException;
+    /**
+     * 尝试获取锁 可以指定尝试时间
+     *    成功：具体方案抽象的实力对象里记录lockId
+     *    失败：记录null
+     * @param tryLockTime 尝试获取时间
+     * @param timeUnit    时间单位
+     *
+     */
+    void tryLock(long tryLockTime, TimeUnit timeUnit);
 
     /**
      * 释放锁
