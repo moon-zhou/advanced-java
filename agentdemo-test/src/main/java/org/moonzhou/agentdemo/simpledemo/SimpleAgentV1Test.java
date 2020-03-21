10f32013490da7e5ce8be725ad19576db1f60f25
moon-zhou <ayimin1989@163.com>
2020-03-21 17:36:45 +0800

    refactor： agent测试单独抽取出来

--- agentdemo-test/src/main/java/org/moonzhou/agentdemo/simpledemo/SimpleAgentV1Test.java
+++ agentdemo-test/src/main/java/org/moonzhou/agentdemo/simpledemo/SimpleAgentV1Test.java
@@ -6,10 +6,12 @@ import java.util.concurrent.TimeUnit;
  * @author moon-zhou <ayimin1989@163.com>
  * @version V1.0.0
  * @description agent测试main方法类
+ *     注意run configuration的VM options：
+ *        <code>-javaagent:E:/CodeSpace/Workspaces/IdeaProjects/advanced-java/agentdemo/target/agentdemo-1.0-SNAPSHOT.jar=moon<code/>
  * @date 2020/3/21 15:30
  * @since 1.0
  */
-public class AgentTest {
+public class SimpleAgentV1Test {
     public static void main(String[] args) throws Exception {
         System.out.println("test agent, main method run start...");
 
