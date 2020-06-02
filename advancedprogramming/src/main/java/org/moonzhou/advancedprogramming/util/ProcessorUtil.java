d79768d058aeddc24824bdc4233f0ebd36e6225c
moon-zhou <ayimin1989@163.com>
2020-06-02 19:14:30 +0800

    feat: 工具类
    获取机器可是用的cpu核数

---
+++ advancedprogramming/src/main/java/org/moonzhou/advancedprogramming/util/ProcessorUtil.java
@@ -0,0 +1,16 @@
+package org.moonzhou.advancedprogramming.util;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈功能详细描述〉
+ *
+ * @author moon-zhou
+ * @date 2020/6/2 19:04
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class ProcessorUtil {
+    public static void main(String[] args) {
+        System.out.println("cpu核数：" + Runtime.getRuntime().availableProcessors());
+    }
+}
