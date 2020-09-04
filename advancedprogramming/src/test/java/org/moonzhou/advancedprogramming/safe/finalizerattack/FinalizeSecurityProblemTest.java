6094e6d88b51c260ff9275273aff02da4982ba88
moon-zhou <ayimin1989@163.com>
2020-09-04 20:00:28 +0800

    feat: finalize绕过解决
    单元测试

--- advancedprogramming/src/test/java/org/moonzhou/advancedprogramming/safe/finalizerattack/FinalizeSecurityProblemTest.java
+++ advancedprogramming/src/test/java/org/moonzhou/advancedprogramming/safe/finalizerattack/FinalizeSecurityProblemTest.java
@@ -4,6 +4,10 @@ package org.moonzhou.advancedprogramming.safe.finalizerattack;
 import org.junit.Test;
 import org.moonzhou.advancedprogramming.safe.finalizerattack.demo001recurringproblem.SensitiveOperation;
 import org.moonzhou.advancedprogramming.safe.finalizerattack.demo001recurringproblem.SensitiveOperationFinalizer;
+import org.moonzhou.advancedprogramming.safe.finalizerattack.demo004logic2fix.SensitiveOperationFlag;
+import org.moonzhou.advancedprogramming.safe.finalizerattack.demo004logic2fix.SensitiveOperationFlagFinalizer;
+import org.moonzhou.advancedprogramming.safe.finalizerattack.demo005jdkadvancedfeature2fix.SensitiveOperationThis;
+import org.moonzhou.advancedprogramming.safe.finalizerattack.demo005jdkadvancedfeature2fix.SensitiveOperationThisFinalizer;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,7 +24,8 @@ public class FinalizeSecurityProblemTest {
     public void testNoException() {
         try {
             // 实例化子类的时候，依然会调用父类的实例化方法，抛出异常，直接进入catch
-            SensitiveOperation sensitiveOperation = new SensitiveOperationFinalizer();
+//            SensitiveOperation sensitiveOperation = new SensitiveOperationFinalizer();
+            SensitiveOperationFinalizer sensitiveOperation = new SensitiveOperationFinalizer();
 
             System.out.println("do store money directly start...");
             sensitiveOperation.storeMoney();
@@ -29,7 +34,54 @@ public class FinalizeSecurityProblemTest {
             System.out.println("error:" + e.getMessage());
         }
 
-        //
+        // gc，触发finalize
+        System.gc();
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testLogic2Fix() {
+
+        try {
+            // 实例化，抛出异常，直接进入catch
+            SensitiveOperationFlag sensitiveOperationFlag = new SensitiveOperationFlagFinalizer();
+
+            System.out.println("do store money directly start...");
+            sensitiveOperationFlag.storeMoney();
+            System.out.println("do store money directly end...");
+        } catch (Exception e) {
+            System.out.println("error:" + e.getMessage());
+        }
+
+        // gc，触发finalize
+        System.gc();
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testThis2Fix() {
+        try {
+            // 实例化，抛出异常，直接进入catch
+            SensitiveOperationThis sensitiveOperationFlag = new SensitiveOperationThisFinalizer();
+
+            System.out.println("do store money directly start...");
+            sensitiveOperationFlag.storeMoney();
+            System.out.println("do store money directly end...");
+        } catch (Exception e) {
+            System.out.println("error:" + e.getMessage());
+        }
+
+        // gc，触发finalize
         System.gc();
 
         try {
