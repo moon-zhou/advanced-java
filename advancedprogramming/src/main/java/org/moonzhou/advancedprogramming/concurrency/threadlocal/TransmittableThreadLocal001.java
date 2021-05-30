501f00e6e4ad2d21ab67e4bbc0d37627fbf9597f
moon-zhou <ayimin1989@163.com>
2021-05-30 08:56:30 +0800

    feat: concurrency
    TransmittableThreadLocal理解补充

--- advancedprogramming/src/main/java/org/moonzhou/advancedprogramming/concurrency/threadlocal/TransmittableThreadLocal001.java
+++ advancedprogramming/src/main/java/org/moonzhou/advancedprogramming/concurrency/threadlocal/TransmittableThreadLocal001.java
@@ -28,11 +28,15 @@ import java.util.concurrent.TimeUnit;
  * i am a transmittable parent
  * i am a new transmittable parent
  *
+ * 现象：
  * 子线程可以获取到父线程设置的值，已经不局限于子线程操作时，只在第一次进行拷贝，
  * 后续如果父线程的只发生了变更，如果再进行子线程的操作，父线程变更的值还是会生效的。而InheritableThreadLocal则不能。
  * 但是子线程设置的值，如果在后续线程池化中复用时，父线程变化后，则子线程修改的值会被父线程的值覆盖。
  * （此规则与上面的规则其实是互斥的，只可选其一，TransmittableThreadLocal or InheritableThreadLocal）
  * 子线程修改的值对于父线程而言，都一样，都是隔离的。
+ *
+ * 原因：
+ * 装饰线程池之后，在每次调用任务的时，都会将当前的主线程的TransmittableThreadLocal数据copy到子线程里面，执行完成后，再清除掉。
  *
  * @author moon-zhou
  * @see [相关类/方法]（可选）
