5151475bd8bb4ac4810fb63df26118b83a361338
moon-zhou <ayimin1989@163.com>
2022-04-21 21:25:31 +0800

    feat: test LocalDateTime

---
+++ advancedprogramming/src/main/java/org/moonzhou/dailyprogramming/datetime/LocalDateTimeTest.java
@@ -0,0 +1,15 @@
+package org.moonzhou.dailyprogramming.datetime;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author moon zhou
+ * @description
+ * @email ayimin1989@163.com
+ * @date 2022/4/20 13:51
+ **/
+public class LocalDateTimeTest {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+}
