235e0130adbe8b9c1acf5a070884805c9f620911
moon-zhou <ayimin1989@163.com>
2022-09-30 16:19:13 +0800

    feat: test DateTimeFormatter

--- advancedprogramming/src/main/java/org/moonzhou/dailyprogramming/datetime/LocalDateTimeTest.java
+++ advancedprogramming/src/main/java/org/moonzhou/dailyprogramming/datetime/LocalDateTimeTest.java
@@ -1,5 +1,6 @@
 package org.moonzhou.dailyprogramming.datetime;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -15,5 +16,14 @@ public class LocalDateTimeTest {
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         System.out.println(dateTimeFormatter.format(LocalDateTime.now()));
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println(dateFormatter.format(LocalDateTime.now()));
+        DateTimeFormatter dateFormatter2 = DateTimeFormatter.ofPattern("MM/yyyy");
+        System.out.println(dateFormatter2.format(LocalDateTime.now()));
+
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM/yyyy")));
+
+        System.out.println(LocalDate.now().getYear());
     }
 }
