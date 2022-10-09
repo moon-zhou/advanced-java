cbe0e9f3c1b8df4d65a282a6adbf27d5554c4ca5
moon-zhou <ayimin1989@163.com>
2022-10-09 21:59:24 +0800

    feat: test array

--- advancedprogramming/src/main/java/org/moonzhou/dailyprogramming/TestArray.java
+++ advancedprogramming/src/main/java/org/moonzhou/dailyprogramming/TestArray.java
@@ -1,5 +1,8 @@
 package org.moonzhou.dailyprogramming;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author moon zhou
  * @version 1.0
@@ -10,5 +13,28 @@ public class TestArray {
     public static void main(String[] args) {
         String[] strArray = {"000"};
         System.out.println(strArray.equals("000"));
+
+        System.out.println("---------------------------------");
+
+        Types types = new Types();
+        String[] typesField = types.getTypes();
+        print(typesField);
+
+        typesField[0] = "000";
+        print(typesField);
+    }
+
+    private static void print(String[] ss) {
+        for (String s : ss) {
+            System.out.println(s);
+        }
+        System.out.println();
+    }
+
+    @Getter
+    @Setter
+    static class Types {
+        // type array is reference type
+        private String[] types = {"111", "222", "333"};
     }
 }
